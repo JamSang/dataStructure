318c3ec38b123a8dc79b17a7c2891660d4c77906
JamSang <sangjam_cn@outlook.com>
2020-09-18 00:16:35 +0800

    2020年9月18日-Day-1-practice

--- class_1_4.py
+++ class_1_4.py
@@ -1,6 +1,31 @@
+def practice_4():
+    last = 0
+    for i in range(10, 1000):
+        n = i
+        a = 200 * n ** 3
+        b = 2 ** n
 
-s = 'abcdefg'
+        print(last)
+        if last/(a-b) < 0:
+
+            print(n)
+            break
+        last = a-b
 
 
-for i in range(len(s)):
-    print(s[i])
+def practice_5():
+    n = 6*10**9
+    t = 24* 60 *60
+
+    a1 = n/t
+    a2 = n*n /t
+    print(a1,a2)
+
+def main():
+    practice_5()
+
+
+
+
+if __name__ == '__main__':
+    main()
